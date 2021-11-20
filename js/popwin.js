db5483a0add6b7366b15483bc37810aecb968cd1
k-j-j <91775574+k-j-j@users.noreply.github.com>
2021-11-20 18:07:13 +0900

    Add files via upload

---
+++ js/popwin.js
@@ -0,0 +1,47 @@
+ var win;
+		function winOpen(){
+				//	win = window.open('http://kangsite.dothome.co.kr/wd18/jungin/m/mobile.html','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=no, width=400, height=600,left=800,top=150');
+	
+		win = window.open('http://kangdot.dothome.co.kr/jhmobile/secondpf.html','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no, scrollbars=yes,width=400, height=600,left=800,top=150');
+		}
+		
+	//http://kangsite.dothome.co.kr/wd18/jungah/m/mobile.html
+			function winOpen1(){	
+		win = window.open('http://kangsite.dothome.co.kr/wd18/jungah/m/mobile.html','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=yes, width=400, height=600,left=800,top=150');
+		}
+		
+		function winOpen2(){	
+		win = window.open('http://kangsite.dothome.co.kr/wd18/cyoung/m/mobile.html ','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=yes, width=400, height=600,left=800,top=150');
+		}
+		
+			function winOpen3(){	
+		win = window.open('http://kangdot.dothome.co.kr/pf01/rwd_yjee/index.html ','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=yes, width=400, height=600,left=800,top=150');
+		}
+		
+				function winOpen4(){	
+		win = window.open('http://kangdot.dothome.co.kr/416finalpf/m_lhb/index.html ','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=yes, width=400, height=600,left=800,top=150');
+		}
+		
+						function winOpen5(){	
+		win = window.open('http://kangdot.dothome.co.kr/210224/m_lyj/mobile_lyj.html ','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=yes, width=400, height=600,left=800,top=150');
+		}
+		
+		
+								function winOpen6(){	
+		win = window.open('http://kangdot.dothome.co.kr/210224/m_ksy/index.html ','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=yes, width=400, height=600,left=800,top=150');
+		}
+		
+		
+		
+										function winOpen7(){	
+		win = window.open('http://kangdot.dothome.co.kr/210224/mobile_LSY/mobile_LSY.html ','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=yes, width=400, height=600,left=800,top=150');
+		}
+		
+		
+		
+				
+										function winOpen8(){	
+		win = window.open('http://kangdot.dothome.co.kr/210224/mobile_lyr/mobile_lyr.html','pf','toolbar=no,location=no,status=no,menubar=no,resizable=no,scrollbars=yes, width=400, height=600,left=800,top=150');
+		}
+		
+		
